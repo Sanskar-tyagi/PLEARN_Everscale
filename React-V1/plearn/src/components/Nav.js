3f91730feb51cbcdda50671b9154e0853c28b5f1
sanskar-tyagi <79279348+Sanskar-tyagi@users.noreply.github.com>
2023-01-08 01:53:07 +0530

    Update Nav.js

---
+++ React-V1/plearn/src/components/Nav.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import logo_plearn from "../assets/Images/Logo_Plearn.png";
+import { ethers } from "ethers";
+import axios from "axios";
+
+export default function Nav({ onUserAccountChange }) {
+  const [userAccount, setUserAccount] = useState(null);
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log("MetaMask Here!");
+
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          accountChangedHandler(result);
+          console.log(result); // Array which contains the accounts.
+          setConnButtonText("Wallet Connected");
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } else {
+      console.log("Need to install MetaMask");
+      alert("Please install MetaMask extension.");
+    }
+  };
+
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setUserAccount(newAccount[0]);
+    onUserAccountChange(newAccount[0]); // Callback function - Passing the value to the parent component which is App.js
+    setWelcomeMessage("Welcome, ");
+  };
+
+  useEffect(() => {
+    if (userAccount != null) {
+      axios.post("http://localhost:8080/", { userAccount }).then((res) => {
+        alert(res.data.message);
+      });
+    }
+  }, [userAccount]);
+
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  };
+
+  // listen for account changes
+  window.ethereum.on("accountsChanged", accountChangedHandler);
+  window.ethereum.on("chainChanged", chainChangedHandler);
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong ">
+        <div className="container-fluid">
+          <div className="logo-thumbnail navbar-brand logo-custom-css">
+            {" "}
+            <img src={logo_plearn} alt="Bootstrap" />
+          </div>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="index.htmlnavbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="index.html"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="index.html">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="index.html">
+                  Tokenomics
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="index.html">
+                  {" "}
+                  Real Estate
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="index.html">
+                  Trading
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="index.html">
+                  {" "}
+                  NFT
+                </a>
+              </li>
+              <li className="nav-item">
+                <button onClick={connectWalletHandler}>{connButtonText}</button>
+              </li>
+            </ul>
+            <span>
+              {welcomeMessage}
+              {userAccount}
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
