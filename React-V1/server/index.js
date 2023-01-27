05f14d41a91f2c7a8ffd03365d47e9e5e4b4d6d3
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-01-27 18:44:02 +0530

    Update index.js

--- React-V1/server/index.js
+++ React-V1/server/index.js
@@ -13,24 +13,24 @@ app.use(express.urlencoded());
 app.use(cors());
 
 //Giving access to itch.io
-app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
+// app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 // app.use(cors({
 //     origin: ['cryptostein.itch.io/','cryptostein.itch.io/plearn', 'itch.io/'],
 //     optionsSuccessStatus: 200 
 // }));
 // app.options("*", cors());
-var allowedOrigins = ['*'];
-app.use(function (req, res, next) {
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
-        // res.setHeader('X-Frame-Options', 'allow');
-        // res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-    });
+// var allowedOrigins = ['*'];
+// app.use(function (req, res, next) {
+//     var origin = req.headers.origin;
+//     if (allowedOrigins.indexOf(origin) > -1) {
+//         res.setHeader("Access-Control-Allow-Origin", origin);
+//         // res.setHeader('X-Frame-Options', 'allow');
+//         // res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+//     }
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+//     });
 
 const router = require("./routes/routes");
 app.use("/", router);
