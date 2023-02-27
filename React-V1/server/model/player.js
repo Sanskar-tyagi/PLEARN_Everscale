548aac8134d4e20bcbc62d02ad4e2773578d475b
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-02-27 16:42:24 +0530

    ownedCharacters array

--- React-V1/server/model/player.js
+++ React-V1/server/model/player.js
@@ -7,7 +7,8 @@ const playerDetailSchema = new mongoose.Schema({
     gameCoins: {type: Number, default: 1000},
     tileNumber: {type: Number, default: 0},
     characterID: {type: Number, default: -1},
-    level: {type: Number, default: 0}
+    level: {type: Number, default: 0},
+    ownedCharacters: {type: [Number], default: [0,1,2,3,4,5]}
 })
 const playerDetail = new mongoose.model("player_detail_table", playerDetailSchema, "player_detail_table");
 
