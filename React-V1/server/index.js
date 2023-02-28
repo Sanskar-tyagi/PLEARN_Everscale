3c8275df60ec49a0162c9ab1229e2fc94056feb3
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-02-28 20:44:19 +0530

    Minor Updates

--- React-V1/server/index.js
+++ React-V1/server/index.js
@@ -12,25 +12,6 @@ app.use(bodyParser.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-//Giving access to itch.io
-// app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
-// app.use(cors({
-//     origin: ['cryptostein.itch.io/','cryptostein.itch.io/plearn', 'itch.io/'],
-//     optionsSuccessStatus: 200 
-// }));
-// app.options("*", cors());
-// var allowedOrigins = ['*'];
-// app.use(function (req, res, next) {
-//     var origin = req.headers.origin;
-//     if (allowedOrigins.indexOf(origin) > -1) {
-//         res.setHeader("Access-Control-Allow-Origin", origin);
-//         // res.setHeader('X-Frame-Options', 'allow');
-//         // res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-//     }
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-//     });
 
 const router = require("./routes/routes");
 app.use("/", router);
@@ -47,15 +28,6 @@ mongoose.connect(mongoURI,
 app.get('/test', (req,res) => {
     res.send("Hello User");
 })
-
-// if(process.env.NODE_ENV == 'production')
-// {
-//     const path = require('path');
-
-//     app.get('/', (req,res) => {
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     })
-// }
 
 //Start the server
 app.listen(process.env.PORT || 8080,() => {
