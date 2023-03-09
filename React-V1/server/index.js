0b6391ab50a834925728d70d79279b94253881bc
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-03-09 14:04:14 +0530

    Update index.js

--- React-V1/server/index.js
+++ React-V1/server/index.js
@@ -30,6 +30,17 @@ app.use(cors());
 //     res.header("Access-Control-Allow-Credentials", "true");
 //     next();
 //     });
+const allowedOrigins = ['https://singular-granita-0e1259.netlify.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const router = require("./routes/routes");
 app.use("/", router);
